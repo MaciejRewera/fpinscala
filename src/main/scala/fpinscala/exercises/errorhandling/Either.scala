0789f4331cf03e828ffe56984a7320e1997dbe22
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2022-10-03 11:45:20 +0100

    [4.6] Implement orElse method for Either

--- src/main/scala/fpinscala/exercises/errorhandling/Either.scala
+++ src/main/scala/fpinscala/exercises/errorhandling/Either.scala
@@ -16,7 +16,9 @@ enum Either[+E,+A]:
     case Left(e) => Left(e)
     case Right(v) => f(v)
 
-  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match
+    case Left(_) => b
+    case Right(v) => Right(v)
 
   def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
     for {

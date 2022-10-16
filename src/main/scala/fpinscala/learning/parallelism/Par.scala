9f0e577a793facc803899c08e3b5022767d897cb
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2022-10-16 12:44:35 +0100

    [7.3] Implement map2Timeouts function for Par
    This function make the two inner Futures respect
    their total timeout value.

--- src/main/scala/fpinscala/learning/parallelism/Par.scala
+++ src/main/scala/fpinscala/learning/parallelism/Par.scala
@@ -1,6 +1,6 @@
 package fpinscala.learning.parallelism
 
-import java.util.concurrent.{Callable, ExecutorService, Future}
+import java.util.concurrent.{Callable, ExecutorService, Future, TimeUnit}
 
 type Par[A] = ExecutorService => Future[A]
 
@@ -8,7 +8,7 @@ object Par:
 
   private case class UnitFuture[A](get: A) extends Future[A] {
     def isDone(): Boolean = true
-    def get(timeout: Long, units: java.util.concurrent.TimeUnit): A = get
+    def get(timeout: Long, units: TimeUnit): A = get
     def isCancelled(): Boolean = false
     def cancel(evenIfRunning: Boolean): Boolean = false
   }
@@ -20,6 +20,32 @@ object Par:
       val a = parA(es)
       val b = parB(es)
       UnitFuture(f(a.get, b.get))
+
+  def map2Timeouts[A, B, C](parA: => Par[A], parB: => Par[B])(f: (A, B) => C): Par[C] =
+    (es: ExecutorService) => new Future[C] {
+      private val futureA = parA(es)
+      private val futureB = parB(es)
+      @volatile private var result: Option[C] = None
+
+      def isDone(): Boolean = result.isDefined
+
+      def get(timeout: Long, units: TimeUnit): A =
+        val timeoutInNanos = TimeUnit.NANOSECONDS.convert(timeout, units)
+        val startTime = System.nanoTime()
+        val resA = futureA.get(timeoutInNanos, TimeUnit.NANOSECONDS)
+        val middleTime = System.nanoTime()
+        val timeLeftForFutureB = timeoutInNanos - (middleTime - startTime)
+        val resB = futureB.get(timeLeftForFutureB, TimeUnit.NANOSECONDS)
+
+        val res = f(resA, resB)
+        result = Some(res)
+        res
+
+      def isCancelled(): Boolean = futureA.isCancelled || futureB.isCancelled
+
+      def cancel(evenIfRunning: Boolean): Boolean =
+        futureA.cancel(evenIfRunning) || futureB.cancel(evenIfRunning)
+    }
 
   def fork[A](par: => Par[A]): Par[A] = (es: ExecutorService) =>
     es.submit(new Callable[A] {

81a503821871524e90ac3d2470d178996282702e
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2023-01-20 21:23:18 +0000

    [10.13] Implement Foldable for Tree

--- src/main/scala/fpinscala/exercises/monoids/Foldable.scala
+++ src/main/scala/fpinscala/exercises/monoids/Foldable.scala
@@ -43,12 +43,16 @@ object Foldable:
   given Foldable[Tree] with
     import Tree.{Leaf, Branch}
     extension [A](as: Tree[A])
-      override def foldRight[B](acc: B)(f: (A, B) => B) =
-        ???
-      override def foldLeft[B](acc: B)(f: (B, A) => B) =
-        ???
+      override def foldRight[B](acc: B)(f: (A, B) => B) = as match
+        case Leaf(value) => f(value, acc)
+        case Branch(l, r) => l.foldRight(r.foldRight(acc)(f))(f)
+
+      override def foldLeft[B](acc: B)(f: (B, A) => B) = as match
+        case Leaf(value) => f(acc, value)
+        case Branch(l, r) => r.foldLeft(l.foldLeft(acc)(f))(f)
+
       override def foldMap[B](f: A => B)(using mb: Monoid[B]): B =
-        ???
+        as.foldLeft(mb.empty)((b, a) => mb.combine(b, f(a)))
 
   given Foldable[Option] with
     extension [A](as: Option[A])

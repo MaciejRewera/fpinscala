bcdc5f448b3a7df5f306dc621dfbbe6208958629
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2023-01-25 20:01:12 +0000

    [11.20] Implement Reader with readerMonad in Monad

--- src/main/scala/fpinscala/exercises/monads/Monad.scala
+++ src/main/scala/fpinscala/exercises/monads/Monad.scala
@@ -136,11 +136,14 @@ object Id:
 opaque type Reader[-R, +A] = R => A
 
 object Reader:
+  def apply[R, A](f: R => A): Reader[R, A] = f
+
   extension [R, A](ra: Reader[R, A])
     def run(r: R): A = ra(r)
 
   given readerMonad[R]: Monad[Reader[R, _]] with
-    def unit[A](a: => A): Reader[R, A] = ???
+    def unit[A](a: => A): Reader[R, A] = Reader(_ => a)
     extension [A](fa: Reader[R, A])
-      override def flatMap[B](f: A => Reader[R, B]) =
-        ???
+      override def flatMap[B](f: A => Reader[R, B]): Reader[R, B] = r =>
+        val a = fa(r)
+        f(a)(r)

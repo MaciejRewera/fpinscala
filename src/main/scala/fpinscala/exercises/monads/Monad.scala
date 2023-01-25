684a595907d91ce90f62fff1eb2edf89c7bb3e58
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2023-01-25 18:53:39 +0000

    [11.17] Implement Id class and idMonad for Monad

--- src/main/scala/fpinscala/exercises/monads/Monad.scala
+++ src/main/scala/fpinscala/exercises/monads/Monad.scala
@@ -124,17 +124,14 @@ object Monad:
 end Monad
 
 case class Id[+A](value: A):
-  def map[B](f: A => B): Id[B] =
-    ???
-  def flatMap[B](f: A => Id[B]): Id[B] =
-    ???
+  def map[B](f: A => B): Id[B] = Id(f(value))
+  def flatMap[B](f: A => Id[B]): Id[B] = f(value)
 
 object Id:
   given idMonad: Monad[Id] with
-    def unit[A](a: => A) = ???
+    def unit[A](a: => A) = Id(a)
     extension [A](fa: Id[A])
-      override def flatMap[B](f: A => Id[B]) =
-        ???
+      override def flatMap[B](f: A => Id[B]) = fa.flatMap(f)
 
 opaque type Reader[-R, +A] = R => A
 

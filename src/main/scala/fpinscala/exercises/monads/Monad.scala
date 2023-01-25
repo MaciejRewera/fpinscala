d82525a9f689e9f7571dfa1b61abe7cd5f27de44
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2023-01-25 18:37:09 +0000

    [11.13] Implement flatMapViaJoinAndMap and composeViaJoinAndMap methods for Monad

--- src/main/scala/fpinscala/exercises/monads/Monad.scala
+++ src/main/scala/fpinscala/exercises/monads/Monad.scala
@@ -71,11 +71,10 @@ trait Monad[F[_]] extends Functor[F]:
   extension [A](ffa: F[F[A]]) def join: F[A] = ffa.flatMap((fa: F[A]) => fa)
 
   extension [A](fa: F[A])
-    def flatMapViaJoinAndMap[B](f: A => F[B]): F[B] =
-      ???
+    def flatMapViaJoinAndMap[B](f: A => F[B]): F[B] = fa.map(f).join
 
   def composeViaJoinAndMap[A, B, C](f: A => F[B], g: B => F[C]): A => F[C] =
-    ???
+    a => f(a).map(g).join
 
 end Monad      
 

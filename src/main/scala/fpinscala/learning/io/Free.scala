011555bd6cfc1ecc3c851d79fcad98e7af0f6c9a
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2023-01-27 16:11:35 +0000

    [13.2] Implement runTrampoline method for Free type

--- src/main/scala/fpinscala/learning/io/Free.scala
+++ src/main/scala/fpinscala/learning/io/Free.scala
@@ -6,18 +6,18 @@ enum Free[+F[_], A]:
   case Return(a: A) extends Free[Nothing, A]
   case Suspend(s: F[A])
   case FlatMap[F[_], A, B](s: Free[F, A], f: A => Free[F, B]) extends Free[F, B]
-  
+
   def flatMap[F2[x] >: F[x], B](f: A => Free[F2, B]): Free[F2, B] = FlatMap(this, f)
-  
+
   def map[B](f: A => B): Free[F, B] = this.flatMap(a => Return(f(a)))
-  
+
   @tailrec
   def step: Free[F, A] = this match
     case FlatMap(FlatMap(x, f), g) => step(x.flatMap(f).flatMap(g))
     case FlatMap(Return(x), f) => step(f(x))
     case _ => this
-    
-  
+
+
 end Free
 
 object Free:
@@ -25,3 +25,13 @@ object Free:
     def unit[A](a: => A): Free[F, A] = Return(a)
     extension [A](fa: Free[F, A])
       override def flatMap[B](f: A => Free[F, B]): Free[F, B] = fa.flatMap(f)
+
+  extension[A] (fa: Free[Function0, A])
+    @tailrec
+    def runTrampoline: A = fa match
+      case Return(a) => a
+      case Suspend(s) => s()
+      case FlatMap(x, f) => x match
+        case Return(a) => f(a).runTrampoline
+        case Suspend(s) => f(s()).runTrampoline
+        case FlatMap(y, g) => y.flatMap(g).flatMap(f).runTrampoline

f93f40450b1a9f4e9fe63d4d7fdd7cb4a78d3605
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2023-02-02 09:03:32 +0000

    [12.1] Implement combinators for Applicative

--- src/main/scala/fpinscala/exercises/applicative/Applicative.scala
+++ src/main/scala/fpinscala/exercises/applicative/Applicative.scala
@@ -20,17 +20,17 @@ trait Applicative[F[_]] extends Functor[F]:
       apply(unit(f))(fa)
 
   def sequence[A](fas: List[F[A]]): F[List[A]] =
-    ???
+    traverse(fas)(identity)
 
   def traverse[A,B](as: List[A])(f: A => F[B]): F[List[B]] =
-    ???
+    as.foldRight(unit(List.empty[B])) { (a, acc) => f(a).map2(acc)(_ :: _) }
 
   def replicateM[A](n: Int, fa: F[A]): F[List[A]] =
-    ???
+    sequence(List.fill(n)(fa))
 
   extension [A](fa: F[A])
     def product[B](fb: F[B]): F[(A, B)] =
-      ???
+      fa.map2(fb)((_, _))
 
     def map3[B, C, D](
       fb: F[B],

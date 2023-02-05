25745df5360a767f9085f20fe42b698110f5b16d
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2023-02-05 19:29:00 +0000

    [12.13] Implement Traverse instances for List, Option and Tree

--- src/main/scala/fpinscala/exercises/applicative/Traverse.scala
+++ src/main/scala/fpinscala/exercises/applicative/Traverse.scala
@@ -58,17 +58,23 @@ object Traverse:
   given listTraverse: Traverse[List] with
     extension [A](as: List[A])
       override def traverse[G[_]: Applicative, B](f: A => G[B]): G[List[B]] =
-        ???
+        val g = summon[Applicative[G]]
+        as.foldRight(g.unit(List.empty[B]))((a, acc) => f(a).map2(acc)(_ :: _))
 
   given optionTraverse: Traverse[Option] with
     extension [A](oa: Option[A])
       override def traverse[G[_]: Applicative, B](f: A => G[B]): G[Option[B]] =
-        ???
+        oa match
+          case Some(a) => f(a).map(Some(_))
+          case None => summon[Applicative[G]].unit(None)
 
   given treeTraverse: Traverse[Tree] = new:
     extension [A](ta: Tree[A])
       override def traverse[G[_]: Applicative, B](f: A => G[B]): G[Tree[B]] =
-        ???
+        val newHead: G[B] = f(ta.head)
+        val newTail: G[List[Tree[B]]] = ta.tail.traverse(t => t.traverse(f))
+        
+        newHead.map2(newTail)((h, t) => Tree(h, t))
   
   given mapTraverse[K]: Traverse[Map[K, _]] with
     extension [A](m: Map[K, A])
